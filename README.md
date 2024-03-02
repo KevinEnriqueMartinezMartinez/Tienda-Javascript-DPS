7b9c2b8a37a41bc80bbfd26224c5aadc1840fba0
KevinEnriqueMartinezMartinez <127909032+KevinEnriqueMartinezMartinez@users.noreply.github.com>
2024-03-01 23:54:04 -0600

    corrección caracteres readme, integracion de contenido dinamico index
    Co-Authored-By: SofiaRojas2890 <129335267+SofiaRojas2890@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Tienda de Zapatos
 
-Este proyecto es una aplicaci�n web de una tienda de zapatos desarrollada utilizando JavaScript. La aplicaci�n proporciona una interfaz interactiva para que los usuarios exploren y compren zapatos.
+Este proyecto es una aplicación web de una tienda de zapatos desarrollada utilizando JavaScript. La aplicación proporciona una interfaz interactiva para que los usuarios exploren y compren zapatos.
 
-## Caracter�sticas
+## Características
 
-- Cat�logo de zapatos con im�genes y descripciones.
+- Catálogo de zapatos con imágenes y descripciones.
 - Funcionalidad de carrito de compras.
 - Proceso de pago simulado.
-- Filtrado por categor�as y tallas.
+- Filtrado por categorías y tallas.
 
-## Tecnolog�as Utilizadas
+## Tecnologías Utilizadas
 
 - JavaScript
 - HTML
@@ -17,27 +17,27 @@ Este proyecto es una aplicaci�n web de una tienda de zapatos desarrollada util
 
 ## Capturas de Pantalla
 
-![Cat�logo](screenshots/catalogo.png)
-_Cat�logo de zapatos con opciones de filtrado._
+![Catálogo](screenshots/catalogo.png)
+_Catálogo de zapatos con opciones de filtrado._
 
 ![Carrito de Compras](screenshots/carrito.png)
 _Vista del carrito de compras con resumen de la orden._
 
-## Instrucciones de Instalaci�n
+## Instrucciones de Instalación
 
 1. Clona el repositorio: `git clone https://github.com/KevinEnriqueMartinezMartinez/Tienda-Javascript-DPS`
 2. Abre el archivo `index.html` en tu navegador.
 
-## Contribuci�n
+## Contribución
 
 Si quieres contribuir, sigue estos pasos:
 
 1. Haz un fork del proyecto.
 2. Crea una nueva rama: `git checkout -b feature/nueva-funcionalidad`
-3. Realiza tus cambios y haz commit: `git commit -m 'A�ade nueva funcionalidad'`
+3. Realiza tus cambios y haz commit: `git commit -m 'Añade nueva funcionalidad'`
 4. Haz push a la rama: `git push origin feature/nueva-funcionalidad`
 5. Abre un pull request.
 
 ## Licencia
 
-Este proyecto est� bajo la Licencia MIT - ver el archivo [LICENSE](LICENSE) para m�s detalles.
+Este proyecto está bajo la Licencia MIT - ver el archivo [LICENSE](LICENSE) para más detalles.
--- js/index.js
+++ js/index.js
@@ -1,3 +1,119 @@
+let items = [
+    {
+      img: "https://letkicks.com/cdn/shop/products/StockXAndre3_86.png?v=1677087684",
+      title: "Nike Jordan 1 Retro High OG Chicago Lost",
+      price: "80.00",
+      stock: 10,
+    },
+    {
+      img: "https://i.ebayimg.com/00/s/MTAwMFgxNDAw/z/PcUAAOSw5epf07Ye/$_58.png",
+      title: "Adidas Yeezy Boost 350",
+      price: "120.00",
+      stock: 8,
+    },
+    {
+      img: "https://images.stockx.com/360/Nike-Dunk-Low-Off-White-Lot-1/Images/Nike-Dunk-Low-Off-White-Lot-1/Lv2/img01.jpg?fm=avif&auto=compress&w=576&dpr=1&updated_at=1635618665&h=384&q=57",
+      title: "Nike Dunk Low Off-White Lot 1",
+      price: "250.00",
+      stock: 5,
+    },
+    {
+      img: "https://res.cloudinary.com/brokenchains/image/upload/v1687587091/Broken-Chains-Air-Jordan-1-Mid-DQ8423-060-03.jpg",
+      title: "Air Jordan 1 Mid Bred",
+      price: "120.00",
+      stock: 10,
+    },
+    {
+      img: "https://deportint.co/cdn/shop/products/CI3899-100_1080x.jpg?v=1678822268",
+      title: "Air Max 270 React",
+      price: "150.00",
+      stock: 20,
+    },
+    {
+      img: "https://static.runnea.com/images/202306/adidas-ultraboost-22-zapatillas-running-400x400x90xX.jpg?1",
+      title: "Ultraboost 22 Running",
+      price: "190.00",
+      stock: 20,
+    },
+    {
+      img: "https://static.nike.com/a/images/t_default/940a4f7c-6e23-481a-8c7b-f474a87a6d56/calzado-de-running-en-carretera-pegasus-39-jXTgc9.png",
+      title: "Air Zoom Pegasus 39 Running",
+      price: "120.00",
+      stock: 25,
+    },
+    {
+      img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/f136e0642dfe4909b3dfab0b00fbe9c9_9366/Torsion_X_Shoes_Grey_EE4885_01_standard.jpg",
+      title: "Torsion X Shoes",
+      price: "90.00",
+      stock: 10,
+    },
+    {
+      img: "https://i8.amplience.net/i/jpl/jd_ML574EVG_a?v=1",
+      title: "New Balance 574 Grey",
+      price: "100.00",
+      stock: 20,
+    },
+    {
+      img: "https://calzacosta.vtexassets.com/arquivos/ids/262258-800-auto?v=638030085514130000&width=800&height=auto&aspect=true",
+      title: "Converse Chuck Taylor All-Stars Black",
+      price: "50.00",
+      stock: 25,
+    },
+    {
+      img: "https://titan22.com/cdn/shop/products/CW2288-111-A_1082x.png?v=1677862917",
+      title: "Nike Air Force 1 Triple White",
+      price: "100.00",
+      stock: 15,
+    },
+    {
+      img: "https://warsawsneakerstore.com/storage/media/f1000/2020/adidas/184388/adidas-stan-smith-white-fx5500-65116a951133c.jpg",
+      title: "Adidas Stan Smith White",
+      price: "80.00",
+      stock: 20,
+    },
+    {
+      img: "https://http2.mlstatic.com/D_NQ_NP_822424-MCO72602924293_102023-O.webp",
+      title: "Vans Old Skool White",
+      price: "90.00",
+      stock: 25,
+    },
+    {
+      img: "https://www.superga-usa.com/cdn/shop/products/superga-unisex-sneakers-white-S000010-901-1_65303681-2d29-47b0-a754-992ec6864f13.jpg?v=1681850338&width=1920",
+      title: "Superga 2750 Cotu Classic White",
+      price: "70.00",
+      stock: 10,
+    },
+    {
+      img: "https://www.lockeroutlet.cl/wp-content/uploads/2023/09/BOLD_VJVX021267B_VIEW3.png",
+      title: "Veja V-10 White",
+      price: "140.00",
+      stock: 5,
+    },
+  ];
+  
+  let containerItems = document.querySelector(".container-items");
+  
+  items.forEach((item) => {
+    let divItems = document.createElement("div");
+    divItems.classList.add("items");
+    divItems.innerHTML = `
+          <figure>
+              <img src="${item.img}" alt="Producto">
+          </figure>
+          <div class="info-product">
+              <h2>${item.title}</h2>
+              <p class="price">Precio: $${item.price}</p>
+              <p class="quantity-available">
+                  Cantidad disponible: <span class="available-quantity">${item.stock}</span>
+              </p>
+              <button class="btn-add-cart">Añadir al carrito</button>
+          </div>
+      `;
+    containerItems.appendChild(divItems);
+  });
+
+
+
 const btnCart = document.querySelector('.container-cart-icon')
 const containerCartProducts = document.querySelector('.container-cart-products')
 const facturaContainer = document.querySelector('.factura-container');
